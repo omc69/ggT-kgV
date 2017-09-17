f758862849561ddacf6e3603c0f512768fde4074
Christian Gehring <cgehring@vmware.com>
2017-09-17 11:51:30 +0200

    Initial Commit

---
+++ ggT und kgVTests/ggT_und_kgVTests.swift
@@ -0,0 +1,36 @@
+//
+//  ggT_und_kgVTests.swift
+//  ggT und kgVTests
+//
+//  Created by Christian Gehring on 17.09.17.
+//  Copyright © 2017 Christian Gehring. All rights reserved.
+//
+
+import XCTest
+@testable import ggT_und_kgV
+
+class ggT_und_kgVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
